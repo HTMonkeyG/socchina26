955dd1d16666e85001428894557aa1bf8e0ab67e
HTMonkeyG <3254339747@qq.com>
2026-06-21 02:59:17 +0800

    feat: Optimize BinaryStream_SerializeBytes().

--- src/shared/binaryStream.c
+++ src/shared/binaryStream.c
@@ -33,36 +33,14 @@ void BinaryStream_SerializeBytes(
 ) {
   if (BinaryStream_Verify(S, size))
     return;
-
-  for (size_t i = 0; i < size; i++) {
-    if (S->mode == BinaryStreamMode_Write)
+  
+  if (S->mode == BinaryStreamMode_Write) {
+    for (size_t i = 0; i < size; i++)
       S->cursor[i] = data[i];
-    else
+  } else {
+    for (size_t i = 0; i < size; i++)
       data[i] = S->cursor[i];
   }
 
   S->cursor += size;
 }
-
-/*
-// We need the compiler to perform type checking on pointers, so we do not
-// use macro declarations. 
-#define BinaryStream_SerializeProto(_Name, _Type) \
-void _Name(\
-  BinaryStream *S,\
-  _Type *data\
-) {\
-  BinaryStream_SerializeBytes(S, (u08 *)data, sizeof(_Type));\
-}
-
-BinaryStream_SerializeProto(BinaryStream_SerializeInt8, i08)
-BinaryStream_SerializeProto(BinaryStream_SerializeUint8, u08)
-BinaryStream_SerializeProto(BinaryStream_SerializeInt16, i16)
-BinaryStream_SerializeProto(BinaryStream_SerializeUint16, u16)
-BinaryStream_SerializeProto(BinaryStream_SerializeInt32, i32)
-BinaryStream_SerializeProto(BinaryStream_SerializeUint32, u32)
-BinaryStream_SerializeProto(BinaryStream_SerializeInt64, i64)
-BinaryStream_SerializeProto(BinaryStream_SerializeUint64, u64)
-BinaryStream_SerializeProto(BinaryStream_SerializeFloat32, f32)
-BinaryStream_SerializeProto(BinaryStream_SerializeFloat64, f64)
-*/
